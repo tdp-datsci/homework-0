53f0bc171eab1207888dfa07f8dd15a4802c7a2f
Terry D Peterson <peterson.terryd@gmail.com>
2019-07-23 16:54:09 -0500

    changes to script to add labels

--- my_first_script.R
+++ my_first_script.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 library(dslabs)
+library(ggrepel)
 data(murders)
 
 murders %>% ggplot(aes(population/10^6, total, label=abb, color = region)) +
   scale_x_log10() +
   scale_y_log10() +
   geom_point() +
-  geom_label_repel() +
+  geom_text_repel() +
   xlab("Population in millions (log scale)") +
   ylab("Total number of murders (log scale)") +
   ggtitle("United States Gun Murders")
