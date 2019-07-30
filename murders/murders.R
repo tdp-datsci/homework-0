55258069225c626d1ce89beb066d69d0094e4bb6
Terry D Peterson <peterson.terryd@gmail.com>
2019-07-29 19:09:10 -0500

    example of R markdown

---
+++ murders/murders.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(dslabs)
+library(ggrepel)
+data(murders)
+
+murders %>% ggplot(aes(population/10^6, total, label=abb, color = region)) +
+  scale_x_log10() +
+  geom_point() +
+  geom_text_repel() +
+  xlab("Population in millions (log scale)") +
+  ylab("Total number of murders") +
+  ggtitle("United States Gun Murders") +
+  ggsave("figs/murders.png")
+
